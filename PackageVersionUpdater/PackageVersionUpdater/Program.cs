d433122669a4d2597581ca5265a47ef833346d12
Viktor Safar <viktor.safar@svea.com>
2019-03-19 15:25:58 +0100

    package version updater

---
+++ PackageVersionUpdater/PackageVersionUpdater/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace PackageVersionUpdater
+{
+    public class Program
+    {
+        private static List<string> _packagesFilesUpdated = new List<string>();
+        private static List<string> _projectFilesUpdated = new List<string>();
+
+        static void Main(string[] args)
+        {
+            SetWindowSize();
+
+            try
+            {
+                var baseDir = GetDirectory();
+                var projectFiles = GetProjectFiles(baseDir);
+                var packagesConfigFiles = GetPackagesConfigFiles(baseDir);
+                var newPackageVersions = GetNewPackageVersions();
+
+                foreach (var newVersion in newPackageVersions)
+                {
+                    Console.WriteLine(Environment.NewLine + $"Updating package '{newVersion.Key}' to version '{newVersion.Value}' . . . ");
+                    UpdateProjectFiles(newVersion, projectFiles);
+                    UpdatePackagesConfigFiles(newVersion, packagesConfigFiles);
+                }
+
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine($"Project files updated: {_projectFilesUpdated.Count}");
+                Console.WriteLine($"Package.config files updated: {_packagesFilesUpdated.Count}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(Environment.NewLine + "Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        private static void SetWindowSize()
+        {
+            try
+            {
+                Console.SetWindowSize(200, 20);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private static string GetDirectory()
+        {
+            Console.WriteLine("Input directory to run in, otherwise the program will run in current directory.");
+            Console.Write("Directory: ");
+            var dir = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(dir))
+            {
+                return System.IO.Directory.GetCurrentDirectory();
+            }
+
+            var dirInfo = new DirectoryInfo(dir);
+            return dirInfo.FullName;
+        }
+
+        private static Dictionary<string, string> GetNewPackageVersions()
+        {
+            if (!System.IO.File.Exists("versions.txt"))
+            {
+                throw new Exception("File 'versions.txt' is required to run this program. See README.txt for details.");
+            }
+
+            var result = new Dictionary<string, string>();
+            var lines = System.IO.File.ReadAllLines("versions.txt");
+
+            foreach (var line in lines)
+            {
+                var split = line.Split(' ');
+                result.Add(split[0], split[1]);
+            }
+
+            return result;
+        }
+
+        private static void UpdatePackagesConfigFiles(KeyValuePair<string, string> newVersion, List<string> packagesConfigFiles)
+        {
+            foreach (var file in packagesConfigFiles)
+            {
+                UpdatePackagesConfigFile(file, newVersion.Key, newVersion.Value);
+            }
+        }
+
+        private static void UpdatePackagesConfigFile(string file, string packageId, string newVersion)
+        {
+            var content = XDocument.Load(file);
+            var elementWithPackage = content.Root.Elements("package").FirstOrDefault(e => e.Attribute("id").Value == packageId);
+            if (elementWithPackage != null)
+            {
+                var currentVersion = elementWithPackage.Attribute("version").Value;
+
+                if (currentVersion != newVersion)
+                {
+                    elementWithPackage.SetAttributeValue("version", newVersion);
+                    content.Save(file);
+                    Console.WriteLine($"Updated package '{packageId}' to version '{newVersion}' in '{file}'");
+                    if (!_packagesFilesUpdated.Contains(file))
+                    {
+                        _packagesFilesUpdated.Add(file);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Package '{packageId}' already at '{newVersion}' in '{file}'");
+                }
+            }
+        }
+
+        private static void UpdateProjectFiles(KeyValuePair<string, string> newVersion, List<string> projectFiles)
+        {
+            foreach (var file in projectFiles)
+            {
+                UpdateProjectFile(file, newVersion.Key, newVersion.Value);
+            }
+        }
+
+        private static void UpdateProjectFile(string file, string packageId, string newVersion)
+        {
+            const string projectNameSpace = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+            var content = XDocument.Load(file);
+            var itemGroupElement = content.Root.Elements($"{{{projectNameSpace}}}ItemGroup").FirstOrDefault(e => e.Elements($"{{{projectNameSpace}}}Reference").Any());
+            if (itemGroupElement != null)
+            {
+                var referenceElement = itemGroupElement.Elements($"{{{projectNameSpace}}}Reference").FirstOrDefault(e => e.Attribute("Include").Value.StartsWith(packageId));
+                
+                if (referenceElement != null)
+                {
+                    var hintPathElement = referenceElement.Elements($"{{{projectNameSpace}}}HintPath").FirstOrDefault();
+                    if (hintPathElement != null)
+                    {
+                        var searchvalue = $"\\packages\\{packageId}.";
+                        var start = hintPathElement.Value.IndexOf(searchvalue);
+                        var end = hintPathElement.Value.IndexOf("\\lib\\", start + searchvalue.Length);
+                        var currentVersion = hintPathElement.Value.Substring(start + searchvalue.Length, end - (start + searchvalue.Length));
+                        var toReplace = $"{searchvalue}{currentVersion}";
+                        var newValue = $"{searchvalue}{newVersion}";
+
+                        if (newValue != toReplace)
+                        {
+                            hintPathElement.SetValue(hintPathElement.Value.Replace(toReplace, newValue));
+                            content.Save(file);
+                            Console.WriteLine($"Updated package '{packageId}' to version '{newVersion}' in '{file}'");
+                            if (!_projectFilesUpdated.Contains(file))
+                            {
+                                _projectFilesUpdated.Add(file);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Package '{packageId}' already at '{newVersion}' in '{file}'");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static List<string> GetProjectFiles(string baseDir)
+        {
+            return Directory.GetFiles(baseDir, "*.csproj", SearchOption.AllDirectories).ToList();
+        }
+
+        private static List<string> GetPackagesConfigFiles(string baseDir)
+        {
+            return Directory.GetFiles(baseDir, "packages.config", SearchOption.AllDirectories).ToList();
+        }
+    }
+}
---
+++ PackageVersionUpdater/PackageVersionUpdater/README.txt
@@ -0,0 +1,23 @@
+﻿(TFS) Branches can use PackageVersionUpdater to keep all projects & solutions in the branch aligned in terms of package versions.
+
+README
+
+This tool updates nuget package versions in
+
+- CSPROJ files
+- packages.config files
+
+This tool runs the update across all files in specified directory and all subdirectories.
+
+1. if it does not exist, create input file "versions.txt" in the tool's current directory
+2. populate the file with package id and the required version, EXAMPLE is below
+  - other packages will not be touched, eg. if you specify MyApp.Models 1.0.3 then only MyApp.Models package version will be set to 1.0.3
+run the tool
+3. as a result, all projects within the specified directory that reference MyApp.Models will now be referencing v1.0.3
+
+EXAMPLE for the versions.txt file:
+
+<package id><space><version>
+MyApp.Models 1.0.3
+MyApp.Client 1.0.2
+
